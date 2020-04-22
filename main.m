7edd6ebaa05140c97e7ad0430e6d7e91fcdc4d2d
megaldamez <60408656+megaldamez@users.noreply.github.com>
2020-04-22 10:54:20 -0400

    edit5

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ no_ele = 1; %total number of elements
 channelSpacing = 0.2; %60/128
 fs = 60e6;
 sampleSpacing = (1/fs)*soundSpeed*1000/2; %sample number vs mm
-Angle = 1:361;
+Angle = 0:45:315;%:360;
 % Angle = [0 90 180 270];
 phantom_positions = [2.75 0 0]/100; %% 55/2 mm %%%%%%%%%%%%%%%%%%
 times = 1;
@@ -32,8 +32,7 @@ imagesc(db(abs(rf_us)))
 post_recon = zeros(401,401);
 for i = 1:401
     for j = 1:401
-        for angle = 1:361
-            
+        for angle = 0:45:315%:360
             zpos = (i - 201);
             ypos = 0;
             xpos = (j - 201) + 0.0275/0.05; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -41,11 +40,11 @@ for i = 1:401
             ydis = ypos * 0.05;
             zdis = zpos * 0.05;
             positions2 = [xdis ydis zdis]/1000;  %  The position of the phantom [m]
-            new_positions2(angle,:) = rotatePhantom(positions2, angle);
-            dis = sqrt(new_positions2(angle,1).^2 + new_positions2(angle,3).^2);
-            pixeldis(angle) = round(dis'*1000/sampleSpacing);
+            new_positions2 = rotatePhantom(positions2, angle);
+            dis = sqrt(new_positions2(:,1).^2 + new_positions2(:,3).^2);
+            pixeldis = round(dis'*1000/sampleSpacing);
             
-            post_recon(j,i) = post_recon(j,i) + (rf_us(pixeldis(angle),angle));
+            post_recon(j,i) = post_recon(j,i) + (rf_us(pixeldis));
             
         end
     end
@@ -61,17 +60,17 @@ env = abs(hilbert(out));
 st = 1;
 ed = 2321;
 % x = [1 size(out,2)]*0.5/times-size(out)*0.5/times/2;
-x = [st size(out,2)-1]*channelSpacing-10;
+x = [st size(out,2)]*channelSpacing;%-10;
 % y = [st size(postBF_F,1)]*sampleSpacing;
 y = [st ed]*sampleSpacing/2;
 env = env/max(max(env(st:ed,:)));
 %%
-figure
+figure(3)
 imagesc(x,y,db(env),[-40 0]);
 % imagesc(abs(env(3000:end-500,:)));
 ylabel('Axial Distance [mm]')
 xlabel('Lateral Distance [mm]')
 colormap(gray)
 axis image
-axis([15 36 3 9])
+% axis([15 36 3 9])
 colorbar
